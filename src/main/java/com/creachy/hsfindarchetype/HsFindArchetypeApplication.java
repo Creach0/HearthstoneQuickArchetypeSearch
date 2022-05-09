80f707390617996f7c71d5e3a05f218d8587a94d
Rory Free <roryfree.au@gmail.com>
2022-05-09 21:57:58 +1000

    Added Spring Initializr Stuff

---
+++ src/main/java/com/creachy/hsfindarchetype/HsFindArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.creachy.hsfindarchetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsFindArchetypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsFindArchetypeApplication.class, args);
+    }
+
+}
